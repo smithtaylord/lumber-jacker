66a51b3628cd2492181f4fa60401863d480c4a8c
Taylor Smith <smith.taylord@gmail.com>
2023-01-27 14:16:25 -0700

    basic game logic functions

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // #region Variables
 let logs = 0
 let clickMultiplier = 0
-let autoMultiplier = 0
+let autoChopAmount = 0
 
 let clickUpgrades = [
     {
-        name: 'axe',
+        name: 'Axe',
         price: 1,
         qty: 0,
         multiplier: 1
     },
     {
-        name: 'super-axe',
+        name: 'Super Axe',
         price: 5,
         qty: 0,
         multiplier: 5
@@ -19,16 +19,18 @@ let clickUpgrades = [
 ]
 let autoUpgrades = [
     {
-        name: 'buddy',
-        price: 1,
+        name: 'Call a Buddy',
+        price: 10,
         qty: 1,
-        multiplier: 2
+        multiplier: 3,
+        isPurchased: false
     },
     {
-        name: 'crew',
-        price: 2,
+        name: 'Call a Crew',
+        price: 100,
         qty: 1,
-        multiplier: 5
+        multiplier: 10,
+        isPurchased: false
     }
 ]
 // #endregion
@@ -36,7 +38,7 @@ let autoUpgrades = [
 // #region create functions
 
 function chopTree() {
-    logs += 1 + clickMultiplier + autoMultiplier
+    logs += 1 + clickMultiplier
     console.log(logs);
     updateLogs()
 }
@@ -59,12 +61,12 @@ function updateLogs() {
 // }
 
 function purchaseClickUpgrade(name) {
-    let foundUpgrade = clickUpgrades.find(upgrade => upgrade.name == name)
-    if (logs >= foundUpgrade.price) {
-        logs -= foundUpgrade.price
-        clickMultiplier += foundUpgrade.multiplier
-        foundUpgrade.qty++
-        foundUpgrade.price += 5
+    let purchasedClickUpgrade = clickUpgrades.find(upgrade => upgrade.name == name)
+    if (logs >= purchasedClickUpgrade.price) {
+        logs -= purchasedClickUpgrade.price
+        clickMultiplier += purchasedClickUpgrade.multiplier
+        purchasedClickUpgrade.qty++
+        purchasedClickUpgrade.price += 5
     } else {
         window.alert('You need more logs!')
     }
@@ -86,12 +88,57 @@ function drawClickUpgradeButtons() {
         <div class="d-flex justify-content-around py-3">
         <h4>${upgrade.qty} / 5 Upgrades</h4>
         <button class="btn btn-success" onclick="purchaseClickUpgrade('${upgrade.name}')">Buy
-        Axe <i class="mdi mdi-pine-tree"></i> ${upgrade.price}</button><h4>+${upgrade.multiplier}</h4>
+        ${upgrade.name} <i class="mdi mdi-pine-tree"></i> ${upgrade.price}</button><h4>+${upgrade.multiplier}</h4>
         </div>
 
     `
     });
     updateButtonElem.innerHTML = template
+}
+
+function purchaseAutoUpgrade(name) {
+    let purchasedAutoUpgrade = autoUpgrades.find(upgrade => upgrade.name == name)
+    if (logs >= purchasedAutoUpgrade.price) {
+        logs -= purchasedAutoUpgrade.price
+        purchasedAutoUpgrade.qty++
+        purchasedAutoUpgrade.price += 5
+        purchasedAutoUpgrade.isPurchased = true
+        autoChopAmount += purchasedAutoUpgrade.multiplier
+    } else {
+        window.alert('You need more logs!')
+    }
+    console.log(purchasedAutoUpgrade);
+    updateAutoUpgradeAmount()
+    drawAutoUpgradeButtons()
+    updateLogs()
+}
+
+function autoCollectLogs() {
+    // console.log(`This is working ${autoChopAmount} added total Logs ${logs}`);
+    logs += autoChopAmount
+    updateLogs()
+}
+
+function updateAutoUpgradeAmount() {
+    let autoUpgradeElem = document.getElementById('auto-upgrade-amount')
+    autoUpgradeElem.innerText = autoChopAmount
+}
+
+function drawAutoUpgradeButtons() {
+    let updateButtonElem = document.getElementById('auto-upgrade-buttons')
+    let template = ''
+    autoUpgrades.forEach(upgrade => {
+        template += `
+        <div class="d-flex justify-content-around py-3">
+        <h4>${upgrade.qty} / 5 Upgrades</h4>
+        <button class="btn btn-success" onclick="purchaseAutoUpgrade('${upgrade.name}')">Buy
+        Axe <i class="mdi mdi-pine-tree"></i> ${upgrade.price}</button><h4>+${upgrade.multiplier}</h4>
+        </div>
+`
+        updateButtonElem.innerHTML = template
+    })
+
+
 }
 
 
@@ -100,5 +147,7 @@ function drawClickUpgradeButtons() {
 // #region function invocation
 updateLogs()
 drawClickUpgradeButtons()
+drawAutoUpgradeButtons()
+setInterval(autoCollectLogs, 3000)
 // #endregion
 
