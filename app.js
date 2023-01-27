b8bacccbd6ccac3fb54b12a9bc615c539549e565
Taylor Smith <smith.taylord@gmail.com>
2023-01-27 11:36:48 -0700

    chop tree and update logs functions gtg

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
-console.log('I\'m Connected!!');
+// #region Variables
+let logs = 0
+
+let clickUpgrades = [
+    {
+        name: 'axe',
+        price: 1,
+        qty: 1,
+        multiplier: 2
+    },
+    {
+        name: 'super-axe',
+        price: 2,
+        qty: 1,
+        multiplier: 5
+    }
+]
+let autoUpgrades = [
+    {
+        name: 'buddy',
+        price: 1,
+        qty: 1,
+        multiplier: 2
+    },
+    {
+        name: 'crew',
+        price: 2,
+        qty: 1,
+        multiplier: 5
+    }
+]
+// #endregion
+
+// #region create functions
 
 function chopTree() {
-    console.log('You Chopped a tree');
+    logs++
+    console.log(logs);
+    updateLogs()
 }
+
+function updateLogs() {
+    let logElem = document.getElementById('log-count')
+    logElem.innerText = logs
+
+}
+
+
+// #endregion
+
+// #region function invocation
+updateLogs()
+
+// #endregion
+
